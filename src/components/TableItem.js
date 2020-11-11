7540259f81c037896f32627d7ca9d23f42f16ea6
JakeGiangan <jakenildo.giangan@sun-asterisk.com>
2020-11-11 14:35:30 +0800

    added <br/> before the Loading items... text

--- src/components/TableItem.js
+++ src/components/TableItem.js
@@ -7,7 +7,7 @@ const TableItem = ({ issues, loading }) => {
       <div class="d-flex justify-content-center">
         <div class="spinner-border" role="status">
           <span class="sr-only"></span>
-        </div>
+        </div> <br />
         <h4>Loading items...</h4>
       </div>
     )
